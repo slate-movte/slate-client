e06c605d691c688715694e0912d60cb7373a7909
hanmango-o <hanmango.o@gmail.com>
2023-07-26 02:12:55 +0900

    refactor : HomeView file name from home-view to home_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:slate/presentation/views/home-view.dart';
+import 'package:slate/presentation/views/home_view.dart';
 
 void main() {
   runApp(const Slate());
