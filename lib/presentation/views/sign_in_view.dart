f5a739c3f122161b98d0527f74971fb58df31647
hanmango-o <hanmango.o@gmail.com>
2023-07-26 02:00:25 +0900

    add : sign with kakao button

--- lib/presentation/views/sign_in_view.dart
+++ lib/presentation/views/sign_in_view.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('SLATE'),
+            Column(
+              children: [
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(327.w, 48.h),
+                  ),
+                  label: const Text('카카오톡으로 로그인'),
+                  icon: const Icon(Icons.abc_rounded),
+                  onPressed: () {
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(builder: (context) => const ),
+                    // )
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
