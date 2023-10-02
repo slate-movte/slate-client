846d8f0d3498e3fac49792b99805c07d06485882
hanmango-o <hanmango.o@gmail.com>
2023-10-03 00:37:40 +0900

    modified:   lib/presentation/views/movie_info_view.dart

--- lib/presentation/views/movie_info_view.dart
+++ lib/presentation/views/movie_info_view.dart
@@ -24,178 +24,181 @@ class MovieInfoView extends StatefulWidget {
 }
 
 class _MovieInfoViewState extends State<MovieInfoView> {
-
   @override
   void initState() {
-    context.read<SearchBloc>().add(
-        MovieInfoSearchEvent(id: widget.item!.movieId!,));
+    context.read<SearchBloc>().add(MovieInfoSearchEvent(
+          id: widget.item!.movieId!,
+        ));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: ColorOf.point.light,
-        child: const Icon(Icons.camera_alt_outlined),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => CameraView()));
-        },
-      ),
-      body: BlocConsumer<SearchBloc, SearchState>(
-        builder: (context, state) {
-          if (state is MovieDataLoaded) {
-            // print("영화정ㅂ"+state.movie.toString());
-            MovieModel movieItem = state.movie as MovieModel;
-            String castingList = movieItem.movieCastList.join(", ");
-            // print(str);
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: ColorOf.point.light,
+          child: const Icon(Icons.camera_alt_outlined),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => CameraView()));
+          },
+        ),
+        body: BlocConsumer<SearchBloc, SearchState>(
+          builder: (context, state) {
+            if (state is MovieDataLoaded) {
+              // print("영화정ㅂ"+state.movie.toString());
+              MovieModel movieItem = state.movie as MovieModel;
+              String castingList = movieItem.movieCastList.join(", ");
+              // print(str);
 
-          return Center(
-            child: ItemTable(
-              header: ItemHeader(
-                actions: [
-                  Padding(
-                    padding: EdgeInsets.only(right: SizeOf.w_md),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.close,
-                        size: 30,
-                        color: ColorOf.white.light,
-                      ),
-                    ),
-                  ),
-                ],
-                forceElevated: true,
-                collapsedHeight: 160.h,
-                expandedHeight: 328.h,
-                flexibleSpace: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment(0.00, -1.00),
-                      end: Alignment(0, 1),
-                      colors: [Colors.black.withOpacity(0.9), Colors.black],
-                    ),
-                  ),
-                  child: FlexibleSpaceBar(
-                    titlePadding: EdgeInsets.symmetric(
-                        horizontal: SizeOf.w_lg, vertical: SizeOf.h_lg),
-                    expandedTitleScale: 1,
-                    centerTitle: false,
-                    title: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return FittedBox(
-                          alignment: Alignment.centerLeft,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Image.network(
-                                movieItem.posterUrl!,
-                                width: 100.w,
-                                height: 144.h,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(left: SizeOf.w_md),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 5.h),
-                                      child: Text(
-                                        movieItem.title,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleSmall!
-                                            .apply(
-                                          color: ColorOf.white.light,
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 3.h),
-                                      child: Text(
-                                        movieItem.company!,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleSmall!
-                                            .apply(
-                                          color: ColorOf.white.light,
-                                        ),
-                                      ),
-                                    ),
-                                    Text(
-                                      '${movieItem.openDate.year!}-${movieItem.openDate.month!}-${movieItem.openDate.day!} 개봉 | ${movieItem.rating}',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleSmall!
-                                          .apply(
-                                        color: ColorOf.white.light,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+              return Center(
+                child: ItemTable(
+                  header: ItemHeader(
+                    actions: [
+                      Padding(
+                        padding: EdgeInsets.only(right: SizeOf.w_md),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.close,
+                            size: 30,
+                            color: ColorOf.white.light,
                           ),
-                        );
-                      },
-                    ),
-                    background: ShaderMask(
-                      shaderCallback: (bound) {
-                        return LinearGradient(
-                          begin: Alignment(0.00, -1.00),
-                          end: Alignment(0, 1),
-                          colors: [Colors.black.withOpacity(0.5), Colors.black],
-                        ).createShader(bound);
-                      },
-                      blendMode: BlendMode.colorBurn,
-                      child: Image.network(
-                        movieItem.posterUrl!,
-                        fit: BoxFit.fitWidth,
-                        alignment: Alignment.topCenter,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              sections: [
-                ItemSection.onlyPost(
-                  builder: ItemSectionBuilder()
-                    ..posts = [
-                      ItemTablePost(
-                        title: '감독',
-                        content: movieItem.director!,
-                      ),
-                      ItemTablePost(
-                        title: '출연',
-                        content: castingList!,
-                      ),
-                      ItemTablePost(
-                        title: '시놉시스',
-                        content: movieItem.plot!,
+                        ),
                       ),
                     ],
+                    forceElevated: true,
+                    collapsedHeight: 160.h,
+                    expandedHeight: 328.h,
+                    flexibleSpace: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment(0.00, -1.00),
+                          end: Alignment(0, 1),
+                          colors: [Colors.black.withOpacity(0.9), Colors.black],
+                        ),
+                      ),
+                      child: FlexibleSpaceBar(
+                        titlePadding: EdgeInsets.symmetric(
+                            horizontal: SizeOf.w_lg, vertical: SizeOf.h_lg),
+                        expandedTitleScale: 1,
+                        centerTitle: false,
+                        title: LayoutBuilder(
+                          builder: (context, constraints) {
+                            return FittedBox(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Image.network(
+                                    movieItem.posterUrl!,
+                                    width: 100.w,
+                                    height: 144.h,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: SizeOf.w_md),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(bottom: 5.h),
+                                          child: Text(
+                                            movieItem.title,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleSmall!
+                                                .apply(
+                                                  color: ColorOf.white.light,
+                                                ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(bottom: 3.h),
+                                          child: Text(
+                                            movieItem.company!,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleSmall!
+                                                .apply(
+                                                  color: ColorOf.white.light,
+                                                ),
+                                          ),
+                                        ),
+                                        Text(
+                                          '${movieItem.openDate!.year}-${movieItem.openDate!.month}-${movieItem.openDate!.day} 개봉 | ${movieItem.rating}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleSmall!
+                                              .apply(
+                                                color: ColorOf.white.light,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                        background: ShaderMask(
+                          shaderCallback: (bound) {
+                            return LinearGradient(
+                              begin: Alignment(0.00, -1.00),
+                              end: Alignment(0, 1),
+                              colors: [
+                                Colors.black.withOpacity(0.5),
+                                Colors.black
+                              ],
+                            ).createShader(bound);
+                          },
+                          blendMode: BlendMode.colorBurn,
+                          child: Image.network(
+                            movieItem.posterUrl!,
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.topCenter,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  sections: [
+                    ItemSection.onlyPost(
+                      builder: ItemSectionBuilder()
+                        ..posts = [
+                          ItemTablePost(
+                            title: '감독',
+                            content: movieItem.director!,
+                          ),
+                          ItemTablePost(
+                            title: '출연',
+                            content: castingList!,
+                          ),
+                          ItemTablePost(
+                            title: '시놉시스',
+                            content: movieItem.plot!,
+                          ),
+                        ],
+                    ),
+                    // ItemSection(
+                    //   builder: ItemSectionBuilder()
+                    //     ..image = const ItemTableGrid(
+                    //       title: '스틸컷',
+                    //     ),
+                    // ),
+                  ],
                 ),
-                // ItemSection(
-                //   builder: ItemSectionBuilder()
-                //     ..image = const ItemTableGrid(
-                //       title: '스틸컷',
-                //     ),
-                // ),
-              ],
-            ),
-          );
-          }
-          return SizedBox(
-            child: SizedBox.shrink(),
-          );
-        },
-        listener: (context, state) {  },
-      )
-    );
+              );
+            }
+            return SizedBox(
+              child: SizedBox.shrink(),
+            );
+          },
+          listener: (context, state) {},
+        ));
   }
 }
